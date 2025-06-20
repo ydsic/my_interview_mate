0762ee0bbb0f481af040faafd232451995f3ce36
ydsic <ydsic99@gmail.com>
2025-06-20 11:44:59 +0900

    fix: Nav 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import MainPage from './page/MainPage';
 import StyleTest from './page/StyleTest';
 import LoginPage from './page/LoginPage';
 import DefaultLayout from './components/layout/DefaultLayout';
+import Nav from './components/common/Nav';
 
 export default function App() {
   return (
     <BrowserRouter basename="/my_interview_mate">
+      <Nav />
       <DefaultLayout>
         <Routes>
           <Route path="/" element={<MainPage />} />
