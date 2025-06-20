b688e2b5bb1d2a85602286c8541f140def7d4334
daj3on9 <dajung5623@naver.com>
2025-06-20 11:48:22 +0900

    Fix : Nav, Default Layout 제거

--- src/page/MyPage.tsx
+++ src/page/MyPage.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import Nav from '../components/common/Nav';
-import DefaultLayout from '../components/layout/DefaultLayout';
 import Dashboard from '../components/mypage/Dashboard';
 import InterviewLog from '../components/mypage/InterviewLog';
 import Favorites from '../components/mypage/Favorites';
@@ -20,39 +18,36 @@ export default function MyPage() {
 
   return (
     <>
-      <Nav />
-      <DefaultLayout>
-        <div className="flex justify-between w-ful px-10 py-3 bg-white rounded-4xl my-10">
-          <div
-            className={`px-5 cursor-pointer ${activeTab === 'dashboard' ? 'text-black' : 'text-gray-400'}`}
-            onClick={() => setActiveTab('dashboard')}
-          >
-            <p> 대시보드</p>
-          </div>
-          <div
-            className={`px-5 cursor-pointer ${activeTab === 'interview' ? 'text-black' : 'text-gray-400'}`}
-            onClick={() => setActiveTab('interview')}
-          >
-            <p> 면접 기록</p>
-          </div>
-          <div
-            className={`px-5 cursor-pointer ${activeTab === 'favorites' ? 'text-black' : 'text-gray-400'}`}
-            onClick={() => setActiveTab('favorites')}
-          >
-            <p> 즐겨 찾기</p>
-          </div>
-          <div
-            className={`px-5 cursor-pointer ${activeTab === 'profile' ? 'text-black' : 'text-gray-400'}`}
-            onClick={() => setActiveTab('profile')}
-          >
-            <p> 프로필</p>
-          </div>
+      <div className="flex justify-between w-ful px-10 py-3 bg-white rounded-4xl my-10">
+        <div
+          className={`px-5 cursor-pointer ${activeTab === 'dashboard' ? 'text-black' : 'text-gray-400'}`}
+          onClick={() => setActiveTab('dashboard')}
+        >
+          <p> 대시보드</p>
         </div>
+        <div
+          className={`px-5 cursor-pointer ${activeTab === 'interview' ? 'text-black' : 'text-gray-400'}`}
+          onClick={() => setActiveTab('interview')}
+        >
+          <p> 면접 기록</p>
+        </div>
+        <div
+          className={`px-5 cursor-pointer ${activeTab === 'favorites' ? 'text-black' : 'text-gray-400'}`}
+          onClick={() => setActiveTab('favorites')}
+        >
+          <p> 즐겨 찾기</p>
+        </div>
+        <div
+          className={`px-5 cursor-pointer ${activeTab === 'profile' ? 'text-black' : 'text-gray-400'}`}
+          onClick={() => setActiveTab('profile')}
+        >
+          <p> 프로필</p>
+        </div>
+      </div>
 
-        <div className=" w-full rounded-4xl px-10 py-3">
-          {tabComponents[activeTab]}
-        </div>
-      </DefaultLayout>
+      <div className=" w-full rounded-4xl px-10 py-3">
+        {tabComponents[activeTab]}
+      </div>
     </>
   );
 }
