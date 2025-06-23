e8fe588de823ff6c6b4d8c15989e7cb35ef1bb43
김호영 <happy3sk@naver.com>
2025-06-23 09:12:32 +0900

    feat: interview page 기본 뼈대 구조 확립

--- src/page/InterviewPage.tsx
+++ src/page/InterviewPage.tsx
@@ -3,10 +3,10 @@ import Button from '../components/common/Button';
 export default function InterviewPage() {
   return (
     <div className="h-full flex flex-col justify-around text-center ">
-      <div>면접 질문 컴포넌트가 들어갈 예정</div>
-      <div>면접 답변 컴포넌트가 들어갈 예정</div>
+      <div className="border">면접 질문 컴포넌트가 들어갈 예정</div>
+      <div className="border">면접 답변 컴포넌트가 들어갈 예정</div>
       <div>
-        <Button>다음질문</Button>
+        <Button className="w-55 h-15">다음 질문</Button>
       </div>
     </div>
   );
