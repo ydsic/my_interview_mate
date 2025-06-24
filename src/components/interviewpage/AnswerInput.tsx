10ae7bfdd3430b6ab1cd7770ddfed179d5a464f6
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-06-24 15:41:21 +0900

    feat: AnswerInput 퍼블리싱 및 공백 입력 방지

--- src/components/interviewpage/AnswerInput.tsx
+++ src/components/interviewpage/AnswerInput.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import addQuestionIcon from '../../assets/ic-add-question.svg';
 import { SubmitButton } from '../common/Button';
 import { H2_content_title } from '../common/HTagStyle';
 import { useState } from 'react';
@@ -5,11 +8,12 @@ import { OpenAIApi } from '../../api/prompt';
 
 export default function AnswerInput() {
   const [answer, setAnswer] = useState('');
-
   const question = 'React의 상태관리는 어떻게 하나요?';
 
+  const isEmpty = answer.trim() === '';
+
   const handleFeedback = async () => {
-    console.log('피드백 요청 시작');
+    if (isEmpty) return;
     try {
       const feedback = await OpenAIApi(question, answer);
       console.log(feedback);
@@ -24,18 +28,36 @@ export default function AnswerInput() {
         <H2_content_title>내 답변</H2_content_title>
       </div>
 
-      <div>
-        <input
-          type="text"
-          className="w-full h-15 pl-4 pr-12 border border-gray-200 rounded-xl focus:outline-none"
-          placeholder="답변을 작성하시거나, 음성으로 대답해주세요."
-          value={answer}
-          onChange={(e) => setAnswer(e.target.value)}
-        />
-      </div>
+      <textarea
+        rows={4}
+        className="w-full min-h-[120px] p-4 border border-gray-200 rounded-xl resize-none focus:outline-none"
+        placeholder="답변을 작성하시거나, 음성으로 대답해주세요."
+        value={answer}
+        onChange={(e) => setAnswer(e.target.value)}
+      />
+
       <div className="flex justify-end gap-4">
-        <SubmitButton onClick={handleFeedback}>피드백 받기</SubmitButton>
-        <button>추가 질문하기</button>
+        <SubmitButton
+          onClick={handleFeedback}
+          className="flex items-center gap-2"
+          disabled={isEmpty}
+        >
+          <FontAwesomeIcon icon={faCheck} className="text-white" size="lg" />
+          피드백 받기
+        </SubmitButton>
+
+        <button
+          disabled={isEmpty}
+          className={`flex items-center gap-2 border border-gray-200 rounded-xl 
+            px-3 py-1 cursor-pointer hover:bg-gray-50 transition ${isEmpty ? 'opacity-50 cursor-not-allowed' : 'hover:bg-gray-50'}`}
+        >
+          <img
+            src={addQuestionIcon}
+            alt="추가 질문하기 아이콘"
+            className="w-4 h-4"
+          />
+          추가 질문하기
+        </button>
       </div>
     </div>
   );
