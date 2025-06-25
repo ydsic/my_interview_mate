301c1462a76836cbdc13590ded52bce0fb77569d
daj3on9 <dajung5623@naver.com>
2025-06-25 17:50:32 +0900

    Fix : Chart에서 사용하는 데이터 props로 받도록 변경

--- src/components/mypage/Dashboard.tsx
+++ src/components/mypage/Dashboard.tsx
@@ -4,12 +4,19 @@ import RadarChart from '../chart/RadarChart';
 import { H2_content_title } from '../common/HTagStyle';
 
 export default function Dashboard(): React.JSX.Element {
-  // 임시 평균 점수 값 80
+  /************ 더미 데이터 ************ */
+  // 평균 점수 데이터
   const avgScore: number = 80;
-  // 임시 총 문제 수 값 47
-  const solvedCount: number = 47;
-  // 임시 최고 점수 값 92
-  const bestScore: number = 92;
+
+  const solvedCount: number = 47; // 임시 총 문제 수 값 47
+  const bestScore: number = 92; // 임시 최고 점수 값 92
+
+  // 라인차트 데이터
+  const lineDays = ['6/13', '6/14', '6/15', '6/16', '6/17', '6/18', '6/19'];
+  const lineData = [70, 82, 85, 92, 88, 75, 60];
+
+  // 역량분석 Radar차트 데이터
+  const RadarData = [80, 85, 87, 88, 90];
 
   return (
     <div className="flex flex-col items-center gap-6 mb-5">
@@ -38,11 +45,11 @@ export default function Dashboard(): React.JSX.Element {
       <section className="flex w-full justify-between gap-3">
         <div className="flex-col bg-white w-full p-5 rounded-4xl shadow-md">
           <p className="font-semibold">점수 변화 추이</p>
-          <LineChart />
+          <LineChart days={lineDays} data={lineData} />
         </div>
         <div className="flex-col items-center bg-white w-full p-5 rounded-4xl shadow-md">
           <p className="font-semibold">역량 분석</p>
-          <RadarChart />
+          <RadarChart data={RadarData} />
         </div>
       </section>
     </div>
