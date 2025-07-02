93350357a5eeb0634e4fec763e6241f21561db94
김호영 <happy3sk@naver.com>
2025-07-02 16:58:09 +0900

    docs: upsert대한 주석 추가

--- src/api/answerAPI.ts
+++ src/api/answerAPI.ts
@@ -6,6 +6,7 @@ export const saveAnswer = async (
   question_id: number,
   content: string,
 ) => {
+  // insert, update 둘 다 가능한 upsert
   const { data, error } = await supabase
     .from('answers')
     .upsert([{ user_id, question_id, content }], {
