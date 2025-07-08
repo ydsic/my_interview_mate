d3194bb389c4e2e33563ba5db16529a57aa3752f
ydsic <ydsic99@gmail.com>
2025-07-08 10:17:42 +0900

    "Revert"

--- src/api/chatgptKey.ts
+++
@@ -1,5 +0,0 @@
-import { supabase } from '../supabaseClient';
-
-export async function chatgptkey() {
-  return await supabase.from('openaikey').select('openai').limit(1).single();
-}
--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl: string = 'https://vlowdzoigoyaudsydqam.supabase.co';
+const supabaseKey: string =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsb3dkem9pZ295YXVkc3lkcWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0MDg3NTUsImV4cCI6MjA1ODk4NDc1NX0.7ltcwu8G4_awXU5SFkAXRGnSeThjTTqAOVUm1bjtmnU';
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
