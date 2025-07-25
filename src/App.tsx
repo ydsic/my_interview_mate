19e4550dfd61929819c502608bb4f93d516fc9f2
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-07-25 15:57:08 +0900

    [refactor/#177] - 관리자 페이지 버튼 클릭시 main 으로 이동하는 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ export default function App() {
               element={<InterviewViewPage />}
             />
             <Route
-              path="/admin"
+              path="/admin/*"
               element={
                 <CheckAdminUuid>
                   <AdminPage />
--- src/components/adminpage/AdminMain.tsx
+++ src/components/adminpage/AdminMain.tsx
@@ -1,10 +1,11 @@
 interface AdminPageProps {
-  onUserClick: () => void;
-  onQuestionClick: () => void;
+  onUserClick?: () => void;
+  onQuestionClick?: () => void;
 }
 
 import { faQuestion, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 export default function AdminMainPage({
   onUserClick,
@@ -12,7 +13,8 @@ export default function AdminMainPage({
 }: AdminPageProps) {
   return (
     <div className="h-full bg-gray-50 flex justify-center items-center gap-8">
-      <div
+      <Link
+        to="user"
         className="w-96 h-96 bg-white border-2 border-gray-200 flex flex-col justify-center items-center rounded-2xl shadow-lg hover:shadow-xl transition-shadow cursor-pointer"
         onClick={onUserClick}
       >
@@ -20,9 +22,10 @@ export default function AdminMainPage({
         <h2 className="text-xl font-semibold text-gray-700 mt-3">
           유저 정보 리스트
         </h2>
-      </div>
+      </Link>
 
-      <div
+      <Link
+        to="question"
         className="w-96 h-96 bg-white border-2 border-gray-200 flex flex-col justify-center items-center rounded-2xl shadow-lg hover:shadow-xl transition-shadow cursor-pointer"
         onClick={onQuestionClick}
       >
@@ -30,7 +33,7 @@ export default function AdminMainPage({
         <h2 className="text-xl font-semibold text-gray-700 mt-3">
           질문 리스트
         </h2>
-      </div>
+      </Link>
     </div>
   );
 }
