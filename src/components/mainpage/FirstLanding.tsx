0911054c2bebffe2ea8cb62ccbe025910db4aa6d
김호영 <happy3sk@naver.com>
2025-08-05 16:03:12 +0900

    [refactor/#189] - firstLanding 페이지 반응형 구현

--- src/components/mainpage/FirstLanding.tsx
+++ src/components/mainpage/FirstLanding.tsx
@@ -17,13 +17,17 @@ export default function FirstLandingPage({ setFirstLandingPage }: Props) {
   return (
     <div
       ref={containerRef}
-      className="absolute left-0 top-0 w-screen min-h-screen flex items-center justify-center"
+      className="absolute inset-0
+      flex items-center justify-center
+      overflow-x-hidden max-sm:overflow-y-auto
+      max-sm:py-20
+      min-h-[120vh]"
     >
       <video
         autoPlay
         muted
         loop
-        className="absolute inset-0 w-full h-full object-cover"
+        className="absolute inset-0 w-full min-h-[120vh] object-cover"
       >
         <source src={bg} type="video/mp4" />
       </video>
@@ -34,23 +38,35 @@ export default function FirstLandingPage({ setFirstLandingPage }: Props) {
         overLight={true}
         elasticity={0}
         cornerRadius={52}
-        padding="100px 70px"
+        padding="130px 60px"
         style={{
           position: 'fixed',
           top: '50%',
           left: '50%',
+          transform: 'translate(-50%, -50%)',
         }}
+        className="      
+          mx-auto 
+          max-sm:py-1                     
+          max-sm:w-[90vw] 
+        "
       >
-        <div className="relative flex flex-col items-center text-center space-y-8 px-6">
-          <div className="flex items-center">
-            <h2 className="text-4xl font-semibold text-blue-200">AI</h2>
-            <h2 className="text-3xl font-semibold text-white">와 함께하는</h2>
+        {/* 카드 내용 */}
+        <div className="flex flex-col items-center text-center space-y-8 max-sm:space-y-6">
+          <div className="flex items-center gap-2">
+            <h2 className="text-4xl font-semibold text-blue-200 max-sm:text-3xl">
+              AI
+            </h2>
+            <h2 className="text-3xl font-semibold text-white max-lg:text-xl max-sm:text-xl">
+              와 함께하는
+            </h2>
           </div>
 
-          <h1 className="text-5xl font-bold bg-gradient-to-r from-white to-blue-200 text-transparent bg-clip-text">
+          <h1 className="text-5xl font-bold bg-gradient-to-r from-white to-blue-200 text-transparent bg-clip-text max-lg:text-3xl max-sm:text-3xl">
             스마트한 면접 준비
           </h1>
-          <p className='className="text-gray-100 font-semibold text-xl leading-relaxed'>
+
+          <p className="font-semibold text-xl leading-relaxed max-lg:text-base max-sm:text-base">
             개인 맞춤형 질문 생성부터 실시간 피드백까지,
             <br />
             AI가 도와주는 체계적인 면접 준비로 꿈의 직장에 한 걸음 더
@@ -74,7 +90,7 @@ export default function FirstLandingPage({ setFirstLandingPage }: Props) {
           >
             <button
               onClick={dontSeeAgain}
-              className="text-white px-10 py-5 rounded-xl font-semibold transition-all duration-300 hover:bg-white/20"
+              className="cursor-pointer text-white px-10 py-5 rounded-xl font-semibold transition-all duration-300 hover:bg-white/20"
             >
               바로가기
             </button>
