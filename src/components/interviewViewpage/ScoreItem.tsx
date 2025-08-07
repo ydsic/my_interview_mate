a2532003ea3105c0d5f3a76b2aae3f2ec6efe7b6
김호영 <happy3sk@naver.com>
2025-08-07 20:42:14 +0900

    [refactor/#189] - 면접 다시보기 화면 반응형 구현

--- src/components/interviewViewpage/ScoreItem.tsx
+++ src/components/interviewViewpage/ScoreItem.tsx
@@ -1,15 +1,18 @@
+import clsx from 'clsx';
+
 type ScoreItemProps = {
   score: number;
   label: string;
+  className?: string;
 };
 
 export default function ScoreItem({ score, label }: ScoreItemProps) {
   const maxScore = 100;
 
   return (
-    <div className="flex flex-1 flex-col items-center w-20 mx-1 border p-2 border-gray-200 rounded-2xl">
-      <p className="text-[28px] font-bold">{score}</p>
-      <div className="w-8 h-32 bg-gray-40 rounded-lg flex items-end">
+    <div className="flex flex-1 flex-col items-center w-20 mx-1 border p-2 border-gray-200 rounded-2xl max-sm:rounded-md">
+      <p className="text-[28px] max-sm:text-[18px] font-bold">{score}</p>
+      <div className="w-8 max-sm:w-4 h-32 max-sm:h-20 bg-gray-40 rounded-lg max-sm:rounded-sm flex items-end">
         <div
           className="w-full bg-gray-100 rounded-lg"
           style={{ height: `${(score / maxScore) * 100}%` }}
